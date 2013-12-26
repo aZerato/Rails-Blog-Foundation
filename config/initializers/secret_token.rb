2acd625b638bbf09fc973e22cc5b36712a9437a6
unknown <jcartoux@CL-4FN505J.ALMERYS.local>
2013-12-26 17:50:20 +0100

    Projet basique foundation rails créé / aucun scaffold fait

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsFoundation::Application.config.secret_key_base = 'ec379bd8e9f85db903f8fe3715259427ab9b5bb9b679a54bb8dd8729648320f1a1fec82e08de6ab988bbafe57b83ea516dfa5acad9993229773fc7e77c9d9adf'
